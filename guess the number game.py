a1f754bf698cb611612924dd0e07877ebc055a51
Emirhan Berber <5w8kpgktgn@privaterelay.appleid.com>
2025-12-23 14:28:17 +0300

    guess the number game

---
+++ guess the number game.py
@@ -0,0 +1,17 @@
+import random
+number=random.randint(0,10)
+point=100
+hak=int(input("How many attempts will it take to find the correct number?:"))
+kırılacak_puan=point/hak
+i=0
+while i<hak:
+    guess=int(input("Guess the number:"))
+    
+    if guess == number:
+        print(f"Congratulations, you guessed correctly.\nYour score is:{point}")
+        break
+    else:
+        point-=kırılacak_puan
+        print(f"Incorrect guess,\nyour score is:{point}")
+    i+=1
+
